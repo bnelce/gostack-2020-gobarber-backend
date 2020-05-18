c101464d06fa783219547a8b2cfe70ca5c288ac1
Abner Oliveira <abner.oliveira.ce@gmail.com>
2020-05-18 11:37:58 -0300

    appointments: migrations, list, create

---
+++ src/database/migrations/1589796137817-CreateAppointments.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateAppointments1589796137817 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
